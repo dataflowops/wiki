6efb4e26780f8f412182f43b78ce65ac09198472
Kanan Rahimov <mail@kenanbek.me>
2024-12-18 23:26:34 +0100

    Deploy website - based on c8418c48ffebe863a1cd8535c38325c5a4db0150

---
+++ assets/js/814f3328.eed0a701.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkwiki=self.webpackChunkwiki||[]).push([[472],{5513:e=>{e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Building AI-Powered Workflows with Data Flow Platform","permalink":"/blog/build-ai-workflows-with-data-flow-platform","unlisted":false,"date":"2024-12-18T22:24:24.000Z"}]}')}}]);
